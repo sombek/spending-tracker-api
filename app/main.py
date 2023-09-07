15a233378e679d5bb7892358e2aedbca717c07e4
Abdullah Hashim <Abdullah97hashim@gmail.com>
2023-09-07 19:28:46 +0300

    add code

--- app/main.py
+++ app/main.py
@@ -1,33 +1,25 @@
-# from fastapi import FastAPI
-# from starlette.middleware.cors import CORSMiddleware
-#
-# from app.budget_breakdown.router import router as budget_breakdown_router
-#
-# app = FastAPI()
-#
-# #  allow CORS
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-#
-#
-# app.include_router(
-#     budget_breakdown_router, prefix="/budget-breakdown", tags=["budget-breakdown"]
-# )
-#
-#
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World"}
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from app.budget_breakdown.router import router as budget_breakdown_router
 
 app = FastAPI()
 
+#  allow CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/api/python")
-def hello_world():
+
+app.include_router(
+    budget_breakdown_router, prefix="/budget-breakdown", tags=["budget-breakdown"]
+)
+
+
+@app.get("/")
+async def root():
     return {"message": "Hello World"}
